944b047e825336ba42aed920778ccb6537c2cba8
Justin Heil <justinheil@limelin.local>
2019-03-24 15:18:00 -0700

    Initial player test suite

--- jasmine/spec/GlobalSpec.js
+++ jasmine/spec/GlobalSpec.js
@@ -1,6 +1,24 @@
 describe('Test suite', function() {
-  console.log('test test test');
   it('test expectation', function() {
     expect(true).toBe(true);
   });
+});
+
+describe('Dev Player test', function() {
+
+  let expected_values = {
+    'initial_armor_rating': 0,
+    'initial_storage_capcity':  10
+  };
+  let fakePlayer = new Player();
+
+  it('Player is initialized with correct storage capacity', function() {
+    //console.log(fakePlayer);
+    expect(fakePlayer.storage_capacity).toBe(expected_values.initial_storage_capcity);
+  });
+  it('Player is initialized with correct armor rating', function() {
+    expect(fakePlayer.armor_rating).toBe(expected_values.initial_armor_rating);
+  });
+
+
 });
