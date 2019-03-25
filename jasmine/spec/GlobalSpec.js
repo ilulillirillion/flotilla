c87b78b4474d448df83699afe65688e47bfd9131
Justin Heil <justinheil@limelin.local>
2019-03-24 17:50:42 -0700

    Dynamic attribute tests

--- jasmine/spec/GlobalSpec.js
+++ jasmine/spec/GlobalSpec.js
@@ -32,16 +32,48 @@ describe('Dev Game test', function() {
 describe('Dev PageController test', function() {})
 
 
+//var test_player_attribute = function(attribute, expected value) {
+//
+//}
+
+
 describe('Dev Player test', function() {
 
   /*
   let expected_values = {
     'initial_armor_rating': 0,
-    'initial_storage_capcity':  10
+    'initial_storage_capacity':  10
   };
   */
+
+  let attribute_tests = {
+    'armor_rating': {
+      '1': 0,
+      '2': 'Armor Rating: 0' },
+    'energy_capacity': {
+      '1': 0,
+      '2': 'Energy Capacity: 0' },
+    'storage_capacity':  {
+      '1': 10,
+      '2': 'Storage Capacity: 10' }
+  };
   let fakePlayer = new Player();
 
+  for (const [attribute, expected_value] of Object.entries(attribute_tests)) {
+    console.log('Testing player attribute');
+    console.log(attribute);
+    console.log(expected_value);
+    it(`Player is initialized with correct ${attribute}`, function() {
+      expect(fakePlayer.attributes[attribute]).toBe(expected_value['1']);
+    });
+
+    it(`Player element-map yields correct ${attribute}`, function() {
+      console.log(`Expected value: ${expected_value['2']}`);
+      expect(fakePlayer.element_map[`player_attribute_${attribute}_p`]).toBe(expected_value['2']);
+    });
+  }
+
+  /*
   it('Player is initialized with correct storage capacity', function() {
     //console.log(fakePlayer);
     console.log('Testing player storage capacity');
@@ -52,7 +84,14 @@ describe('Dev Player test', function() {
   it('Player is initialized with correct armor rating', function() {
     expect(fakePlayer.attributes.armor_rating).toBe(expected_values.player_initial_armor_rating);
   });
+  it('Player is initialized with correct energy capacity', function() {
+    expect(fakePlayer.attributes.energy_capacity).toBe(expected_values.player_initial_energy_capacity);
+  });
+  */
 
+
+
+  /*
   it('Player element-map yields correct armor rating', function() {
     expect(fakePlayer.element_map.player_attribute_armor_rating_p).toBe(
       `Armor Rating: ${expected_values.player_initial_armor_rating}`);
@@ -61,6 +100,11 @@ describe('Dev Player test', function() {
     expect(fakePlayer.element_map.player_attribute_storage_capacity_p).toBe(
       `Storage Capacity: ${expected_values.player_initial_storage_capacity}`);
   });
+  it('Player element-map yields correct storage capacity', function() {
+    expect(fakePlayer.element_map.player_attribute_energy_capacity_p).toBe(
+      `Energy Capacity: ${expected_values.player_initial_energy_capacity}`);
+  })
+  */
 
 
 });
