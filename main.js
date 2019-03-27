8f0236c8f380f99abd9dd697591287355ded4b9a
Justin Heil <justinheil@limelin.local>
2019-03-26 19:10:57 -0700

    Code cleanup

--- main.js
+++ main.js
@@ -33,7 +33,6 @@ class Attribute {
     this.name = name;
     this.display_name = display_name || name;
     this.value = value;
-    //this.maximum_value = maximum_value;
   }
 
   get dynamic_document_node() {
@@ -59,123 +58,7 @@ class Player {
     };
     this.attributes = attributes;
 
-    /*
-    let raw_attributes = [
-      new PlayerAttribute('armor_rating'),
-      new PlayerAttribute('storage_capacity'),
-      new PlayerAttribute('current_energy'),
-      new PlayerAttribute('energy_capacity')
-    ];
-    this.raw_attributes = raw_attributes;
-    */
-
-    // Represent ship parts and upgrades as 'components'
-    /*
-    let components = [
-      new Component(
-          'satellite_hull',
-          {'storage_capacity': 10})
-    ];
-    this.components = components;
-    */
-
   }
-
-  /*
-  get attributes() {
-
-    let attributes = {};
-
-
-    Game.player.attributes.forEach(function(attribute) {
-      adjusted_attributes[attribute.name] = (adjusted_attributes[attribute.name] || 0) + attribute.value;
-    });
-
-    this.components.forEach(function(component) {
-      //Object.keys(component.modifiers).forEach(function(attribute) {
-      for (const [attribute, value] of Object.entries(component.modifiers)) {
-        adjusted_attributes[attribute] = (adjusted_attributes[attribute] || 0) + value;
-      }
-    });
-
-  }
-  */
-
-  /*
-  get element_map() {
-    console.log('Deriving element map');
-    let element_map = {};
-
-    //let attributes = this.attributes;
-    let attributes = this.adjusted_attributes;
-    console.log(attributes);
-    for (const [attribute, value] of Object.entries(attributes)) {
-      console.log(`Iterating attribute: <${attribute}>`);
-      let element_name = String(`player_attribute_${attribute}_p`);
-      let inner_html_attribute = String(attribute.split('_').join(' '));
-      // https://stackoverflow.com/questions/2332811/capitalize-words-in-string
-      inner_html_attribute = inner_html_attribute.replace(/\b\w/g, l => l.toUpperCase());
-      //inner_html_attribute = inner_html_attribute.charAt(0).toUpperCase() + inner_html_attribute.
-      //inner_html_attribute = inner_html_attribute.charAt(0).toUpper
-      let inner_html = String(`${inner_html_attribute}: ${value}`);
-      element_map[element_name] = inner_html;
-    }
-
-    return element_map;
-  }
-  */
-
-  /*
-  get adjusted_attributes() {
-    //let attributes = {
-    //  'armor_rating': 0,
-    //  'storage_capacity': 0
-    //};
-    console.log('deriving attributes');
-
-    //let attributes = {};
-
-    //let attributes = this.base_attributes;
-
-    let adjusted_attributes = {};
-
-    //console.log(attributes);
-    //console.log(Player.base_attributes);
-
-    //for (const [attribute, value] of Object.entries(Game.player.attributes)) {
-    Game.player.attributes.forEach(function(attribute) {
-      adjusted_attributes[attribute.name] = (adjusted_attributes[attribute.name] || 0) + attribute.value;
-    });
-
-    this.components.forEach(function(component) {
-      //Object.keys(component.modifiers).forEach(function(attribute) {
-      for (const [attribute, value] of Object.entries(component.modifiers)) {
-        adjusted_attributes[attribute] = (adjusted_attributes[attribute] || 0) + value;
-      }
-    });
-
-
-    console.log('Finished deriving attributes');
-    console.log(adjusted_attributes);
-    return adjusted_attributes;
-  }
-  */
-
-  /*
-  get armor_rating() {
-
-    let total = this.components.reduce((a, b) => a + (b['armor_rating'] || 0), 0);
-    return total;
-
-  }
-  */
-
-  /*
-  get storage_capacity() {
-    let total = this.components.reduce((a, b) => a + (b['storage_capacity'] || 0), 0);
-    return total;
-  }
-  */
 
 }
 
@@ -190,36 +73,19 @@ class PageController {
     document.body.append(player_info_div);
 
 
-    //Object.keys(Player.base_attributes).forEach(function(attribute) {
-    //Object.keys(Game.player.attributes).forEach(function(attribute) {
     Object.values(Game.player.attributes).forEach(function(attribute) {
 
       let element = document.createElement(attribute.dynamic_document_node.element_type);
       element.setAttribute('id', attribute.dynamic_document_node.element_id);
-      //attribute.dynamic_document_node.parent
       let parent = document.getElementById(attribute.dynamic_document_node.parent_id);
       parent.appendChild(element);
 
-      /*
-      let paragraph = document.createElement('p');
-      paragraph.setAttribute('id', `player_attribute_${attribute.name}_p`);
-      player_info_div.appendChild(paragraph);
-      */
     })
 
   }
 
   static update_document() {
     console.log('updating document');
-    //console.log(id_value_map);
-    /*
-    for (const [element_id, inner_html] of Object.entries(Game.player.element_map)) {
-      console.log(element_id);
-      console.log(inner_html);
-      let element = document.getElementById(element_id);
-      element.innerHTML = inner_html;
-    }
-    */
 
     // Attributes
     Object.values(Game.player.attributes).forEach(function(attribute) {
@@ -236,13 +102,6 @@ class Game {
   static player = new Player();
   static page_controller = PageController;
 
-  /*
-  static id_value_map = {
-    'player_attribute_armor_rating_p': String(`Armor Rating: ${Game.player.armor_rating}`),
-    'player_attribute_storage_capacity_p': String(`Storage Capacity: ${Game.player.storage_capacity}`)
-  };
-  */
-  
 }
 
 
@@ -254,5 +113,4 @@ window.onload = function() {
   Game.page_controller.initialize_document();
   Game.page_controller.update_document(Game.id_value_map);
   console.log(Game.player.attributes);
-  //console.log(Game.player.element_map);
 }
