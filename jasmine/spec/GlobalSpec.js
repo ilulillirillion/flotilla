06d5e03acb62f8f9abab2a1698fe4c5f08af88ee
Justin Heil <justinheil@limelin.local>
2019-03-26 19:57:16 -0700

    Add game loop jasmine simple tests

--- jasmine/spec/GlobalSpec.js
+++ jasmine/spec/GlobalSpec.js
@@ -14,14 +14,25 @@ class SimpleTest {
 
 describe('Master Test Suite', function() {
 
-  test_player = new Player();
+  let test_player = new Player();
+  console.log(test_player);
+  let test_world = new World();
+  console.log(test_world);
 
   simple_tests = [
-    new SimpleTest(
-        'Hull integrity has correct initial value',
-        //test_player.attributes.find())
-        test_player.attributes.hull_integrity.value,
-        0)
+      new SimpleTest(
+          'Player hull integrity has correct initial value',
+          //test_player.attributes.find())
+          test_player.attributes.hull_integrity.value,
+          0),
+      new SimpleTest(
+          'Player energy has correct initial value',
+          test_player.attributes.energy.value,
+          0),
+      new SimpleTest(
+          'World epoch seconds has correct initial value',
+          test_world.epoch_seconds.value,
+          0)
   ];
 
   simple_tests.forEach(function(test) {
