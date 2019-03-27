645309ede9b0fa39c288809a8926e6911f53b2b3
Justin Heil <justinheil@limelin.local>
2019-03-27 11:36:56 -0700

    Return element and parament from dynamic nodes

--- main.js
+++ main.js
@@ -11,6 +11,33 @@ class Component {
 }
 
 
+/*
+class EnergyMeasurement {
+  constructor(value) {
+    this.raw_value = value;
+  }
+
+  // TODO: Why doesn't this work?
+  //toString() {
+    //console.log(Number(this.raw_value.toFixed(2)));
+  //  return Number((this.raw_value).toFixed(1));
+  //}
+
+  //valueOf() {
+  //  return this.raw_value;
+  //}
+
+  //get value() {
+  //  console.log('getting value')
+  //  let value = parseFloat(this.raw_value.toFixed(2));
+  //  return value;
+  //}
+}
+*/
+
+
+
+
 class DynamicDocumentNode {
   constructor(name, value, element_type, element_id, parent_id, inner_html_name) {
     this.name = name;
@@ -21,7 +48,24 @@ class DynamicDocumentNode {
     this.inner_html_name = inner_html_name;
   }
 
+  get parent() {
+    let parent = document.getElementById(this.parent_id);
+    return parent;
+  }
+
+  get element() {
+    let element = document.getElementById(this.element_id);
+    if (!element) {
+      element = document.createElement(this.element_type);
+      this.parent.appendChild(element);
+    }
+    element.setAttribute('id', this.element_id);
+    return element;
+
+  }
+
   get inner_html() {
+    console.log('getting inner_html');
     let inner_html = String(`${this.inner_html_name}: ${this.value}`);
     return inner_html;
   }
@@ -56,6 +100,7 @@ class Player {
     let attributes = {
       'hull_integrity': new Attribute('hull_integrity', 0, 'Hull Integrity'),
       'energy': new Attribute('energy', 0, 'Energy')
+      //'energy': new Attribute('energy', new EnergyMeasurement(0), 'Energy')
     };
     this.attributes = attributes;
 
@@ -65,6 +110,7 @@ class Player {
 
     // Player gains 0.001 energy every tick
     this.attributes.energy.value += 0.001;
+    //this.attribute.energy.value.raw_value += 0.001;
 
   }
 
@@ -81,12 +127,14 @@ class PageController {
     document.body.append(player_info_div);
 
 
+    /*
     Object.values(Game.player.attributes).forEach(function(attribute) {
       let element = document.createElement(attribute.dynamic_document_node.element_type);
       element.setAttribute('id', attribute.dynamic_document_node.element_id);
       let parent = document.getElementById(attribute.dynamic_document_node.parent_id);
       parent.appendChild(element);
     });
+    */
 
     // World second ticker
     let element = document.createElement(Game.world.epoch_seconds.element_type);
@@ -101,10 +149,14 @@ class PageController {
 
     // Attributes
     Object.values(Game.player.attributes).forEach(function(attribute) {
-      let element = document.getElementById(attribute.dynamic_document_node.element_id);
-      let parent = document.getElementById(attribute.dynamic_document_node.parent_id);
+      //let element = document.getElementById(attribute.dynamic_document_node.element_id);
+      //let element = document.getElementById(attribute.dynamic_document_node.element);
+      //let parent = document.getElementById(attribute.dynamic_document_node.parent_id);
+      //let parent = document.getElementById(attribute.dynamic_document_node.parent);
       let inner_html = attribute.dynamic_document_node.inner_html;
-      element.innerHTML = inner_html;
+      console.log(inner_html);
+      //element.innerHTML = inner_html;
+      attribute.dynamic_document_node.element.innerHTML = inner_html;
     });
 
     // World Epoch Seconds
@@ -112,6 +164,7 @@ class PageController {
     let element = document.getElementById(Game.world.epoch_seconds.element_id);
     //let parent = document.getElementById(Game.world.epoch_seconds.dynamic_document_node.parent_id);
     let inner_html = Game.world.epoch_seconds.inner_html;
+    console.log(inner_html);
     element.innerHTML = inner_html;
 
   }
