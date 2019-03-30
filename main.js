d8cac31c13ff247d1d14133cd72ad1281b2f92c4
Justin Heil <justinheil@limelin.local>
2019-03-29 19:25:22 -0700

    Many regressions, but much better dynamic nodes

--- main.js
+++ main.js
@@ -1,4 +1,151 @@
-class Component {
+class DocumentNode {
+  constructor({
+    element_id,
+    value = '',
+    decoration = '',
+    element_type = 'p',
+    parent_id = null,
+    linebreak = true
+  }) {
+
+    this.element_id = element_id;
+    this.value = value;
+    this.decoration = decoration;
+    this.element_type = element_type;
+    this.parent_id = parent_id;
+
+    // Create the element on instantiation
+    let element = document.getElementById(element_id);
+    if (!element) {
+      console.log('Creating new element for DocumentNode');
+      element = document.createElement(this.element_id);
+      this.parent.appendChild(element);
+      console.log(`Setting element id to ${this.element_id}`);
+      element.setAttribute('id', this.element_id);
+      element.innerHTML = this.inner_html;
+      if (linebreak) {
+        let br = document.createElement('br');
+        this.parent.appendChild(br);
+        //console.log(parent);
+      }
+    }
+  }
+
+  get parent() {
+    // Note: https://jsperf.com/unique-by-tag-name
+    let parent = document.body;
+    if (this.parent_id) {
+      // Simply trust that the parent exists
+      parent = document.getElementById(this.parent_id);
+    }
+    return parent;
+  }
+
+  get element() {
+    let element = document.getElementById(this.element_id);
+    // If the element doesn't exist, create it
+    if (!element) {
+      element = document.createElement(this.element_id);
+      this.parent.appendChild(element);
+    }
+    // Always set the ID while updating element
+    element.setAttribute('id', this.element_id);
+    // Finally, just return the element
+    return element;
+  }
+
+  get inner_html() {
+    console.log('Retrieving DocumentNode inner_html');
+    let inner_html = '';
+    //if (this.decoration.isArray()) {
+    if (Array.isArray(this.decoration)) {
+      inner_html = this.decoration[0];
+      inner_html += String(this.value);
+      inner_html += this.decoration[1];
+    }
+    else {
+      inner_html = this.value;
+    }
+    console.log(inner_html);
+    return inner_html;
+  }
+
+  tick() {
+    console.log('Ticking DocumentNode');
+    let inner_html = this.inner_html;
+    this.element.innerHTML = inner_html;
+  }
+}
+
+
+class ____GenericDocumentNode {
+  constructor({
+    element_id,
+    value = null,
+    element_type = 'p',
+    parent_id = null    
+  }) {
+
+    // Create the element on instantiation
+    element = document.getElementById(element_id);
+    if (!element) {
+      element = document.createElement(this.element_id);
+      this.parent.appendChild(element);
+      element.setAttribute('id', this.element_id);
+    }
+  }
+
+  get parent() {
+    // Simply trust that the parent exists
+    let parent = document.getElementById(this.parent_id);
+    return parent;
+  }
+
+  get element() {
+    let element = document.getElementById(this.element_id);
+    // If the element doesn't exist, create it
+    if (!element) {
+      element = document.createElement(this.element_id);
+      this.parent.appendChild(element);
+    }
+    // Always set the ID while updating element
+    element.setAttribute('id', this.element_id);
+    // Finally, just return the element
+    return element;
+  }
+  get inner_html() {
+    inner_html = this.value;
+    return inner_html;
+  }
+
+  tick() {
+    this.element.innerHTML = this.value;
+  }
+}
+
+
+/*
+class ___CountNode {
+  constructor({
+      element_id,
+      value = null,
+      value_prefix = '',
+      value_suffix = '',
+      value_ceiling = null,
+      element_type = 'p',
+      parent_id = null
+  }) {
+    super();
+
+  }
+}
+*/
+
+
+
+/////////////////////////////////////
+
+class ___Component {
 
   constructor(name='component', modifiers={}) {
     this.name = name
@@ -10,9 +157,9 @@ class Component {
   }
 }
 
-
 //IDEA: create element on document in constructor?
-class GenericDocumentNode {
+/*
+class ___GenericDocumentNode {
   constructor(
       value = '',
       element_type = 'p',
@@ -44,9 +191,11 @@ class GenericDocumentNode {
     this.element.innerHTML = this.inner_html;
   }
 }
+*/
 
 
-class CountNode extends GenericDocumentNode {
+/*
+class ___CountNode extends GenericDocumentNode {
 
   // Think this needs to be psuedo named to work
   constructor(
@@ -62,9 +211,10 @@ class CountNode extends GenericDocumentNode {
 
   }
 }
+*/
 
 
-class DynamicDocumentNode {
+class ___DynamicDocumentNode {
   constructor(name, value, element_type, element_id, parent_id, inner_html_name, inner_html_format_macro=null) {
     this.name = name;
     this.value = value;
@@ -113,6 +263,16 @@ class Attribute {
     this.value_format_macro = value_format_macro;
   }
 
+  get document_node() {
+    let document_node = new DocumentNode({
+      element_id: String(`player_attribute_${this.name}_p`),
+      value: this.value,
+      parent_id: 'player_info_div'
+    });
+    return document_node;
+  }
+
+  /*
   get dynamic_document_node() {
     let element_type = String('p');
     let dynamic_document_node = new DynamicDocumentNode(
@@ -125,9 +285,11 @@ class Attribute {
         this.value_format_macro);
     return dynamic_document_node;
   }
+  */
 }
 
 
+/*
 class Action {
   constructor() {
   }
@@ -140,6 +302,7 @@ class Action {
     )
   }
 }
+*/
 
 
 class Player {
@@ -172,10 +335,12 @@ class PageController {
     document.body.append(player_info_div);
 
     // World second ticker
+    /*
     let element = document.createElement(Game.world.epoch_seconds.element_type);
     element.setAttribute('id', Game.world.epoch_seconds.element_id);
     let parent = document.getElementById(Game.world.epoch_seconds.parent_id);
     parent.appendChild(element);
+    */
 
   }
 
@@ -184,11 +349,15 @@ class PageController {
 
     // Attributes
     Object.values(Game.player.attributes).forEach(function(attribute) {
-      let inner_html = attribute.dynamic_document_node.inner_html;
-      console.log(inner_html);
-      attribute.dynamic_document_node.element.innerHTML = inner_html;
+      //let inner_html = attribute.dynamic_document_node.inner_html;
+      //let inner_html = attribute.document_node.inner_html;
+      //console.log(inner_html);
+      //attribute.dynamic_document_node.element.innerHTML = inner_html;
+
+      attribute.document_node.tick();
     });
 
+    /*
     // World Epoch Seconds
     console.log(Game.world.epoch_seconds);
     let element = document.getElementById(Game.world.epoch_seconds.element_id);
@@ -196,6 +365,7 @@ class PageController {
     let inner_html = Game.world.epoch_seconds.inner_html;
     console.log(inner_html);
     element.innerHTML = inner_html;
+    */
 
   }
 }
@@ -203,6 +373,7 @@ class PageController {
 
 class World {
   constructor() {
+    /*
     this.epoch_seconds = new DynamicDocumentNode(
         'epoch_seconds',
         0,
@@ -210,10 +381,11 @@ class World {
         'world_epoch_seconds',
         'player_info_div',
         'World Age');
+    */
   }
 
   tick() {
-    this.epoch_seconds.value += 1;
+    //this.epoch_seconds.value += 1;
   }
 }
 
